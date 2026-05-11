6c244d62917c65d301b5dd36118297b1d7a7b821
Michael Cheers <michaelcheers@users.noreply.github.com>
2026-05-11 03:29:48 -0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React, { Suspense } from 'react';
 import Layout from './components/Layout.jsx';
 import Router from './router/Router.jsx';
-import { AddressProvider } from './contextValues.jsx';
+import { AddressProvider } from './app/contextValues.jsx';
 import UtmHandler from './app/components/utmcomponent.jsx';
 import AnalyticsProvider from './app/components/AnalyticsProvider.jsx';
 import VancouverHeartbeatTracker from './app/components/VancouverHeartbeatTracker.jsx';
